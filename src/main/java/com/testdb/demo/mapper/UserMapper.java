b07ef6dd39f790d88c5c3ba8a474b12756442dc3
LiuZhen <1204736871@qq.com>
2020-04-02 18:35:48 +0800

    MYSQL统一所有变量名为小写

--- src/main/java/com/testdb/demo/mapper/UserMapper.java
+++ src/main/java/com/testdb/demo/mapper/UserMapper.java
@@ -15,13 +15,13 @@ public interface UserMapper extends BaseMapper<User> {
 //    @Insert("INSERT INTO USER(NAME, AGE, ADDRESS) VALUES(#{name}, #{age}, #{address})")
 //    int insert(@Param("name") String name, @Param("age") Integer age, @Param("address")String address);
 
-    @Select("SELECT * FROM USER WHERE USERNAME=#{username}")
+    @Select("SELECT * FROM user WHERE username=#{username}")
     User selectByUsername(@Param("username") String username);
 
-    @Insert("INSERT INTO USER_ROLE(USER_ID, ROLE_ID) VALUES(#{user_id}, #{role_id})")
+    @Insert("INSERT INTO user_role(user_id, role_id) VALUES(#{user_id}, #{role_id})")
     Boolean createUser(@Param("user_id") String user_id, @Param("role_id") String role_id);
 
-    @Select("SELECT ID FROM USER WHERE USERNAME=#{username}")
+    @Select("SELECT id FROM user WHERE username=#{username}")
     int selectIdByUsername(@Param("username") String username);
 
     User selectUserAndRoles(@Param("username") String username);
