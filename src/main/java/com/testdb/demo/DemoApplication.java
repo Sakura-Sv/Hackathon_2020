1068fed5c9ba5044a6996430667996857ce81058
LiuZhen <1204736871@qq.com>
2020-04-07 16:49:17 +0800

    修改了入口函数，删了些没用的

--- src/main/java/com/testdb/demo/DemoApplication.java
+++ src/main/java/com/testdb/demo/DemoApplication.java
@@ -1,28 +1,16 @@
 package com.testdb.demo;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-import javax.annotation.PostConstruct;
-
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableAsync(proxyTargetClass = true)
 public class DemoApplication {
-
-    @Autowired
-    private ObjectMapper objectMapper;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @PostConstruct
-    public void setUp() {
-        objectMapper.registerModule(new JavaTimeModule());
-    }
 }
